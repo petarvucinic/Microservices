9e9a7b0a055257c0df80541424bd5d30366f7654
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2023-02-14 19:48:21 +0100

    Products CRUD

--- inventory/main.py
+++ inventory/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-# from redis import get_redis_connection, HashModel
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
+from pydantic import BaseModel
+
+
 app = FastAPI()
 
-# app.add_middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+
+)
 
 redis = get_redis_connection(
     host="redis-13116.c240.us-east-1-3.ec2.cloud.redislabs.com",
@@ -22,8 +31,27 @@ class Product():
 
 @app.get('/products')
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
 
 
+@app.post('/products')
+def create(product: Product):
+    return product.save()
 
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
 
+@app.delete("/products/{pk}")
+def delete(pk: str):
+    return product.delete(pk)
